6fd64965bca0bc70a04038e6a4dd7324a0c775bf
deanstein <jdeangoldstein@gmail.com>
2022-11-26 20:07:33 -0700

    Basic component setup

---
+++ src/ui-modules/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<div class='header' id='test!!!!!!!!!!!'>
+  This is a header! 
+</div>
+
+<style>
+  .header {
+    background-color: grey;
+    height: 5%;
+  }
+</style>
