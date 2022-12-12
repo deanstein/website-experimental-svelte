3f49faf7e91d6151c082b213ac56bef9d00b008f
deanstein <jdeangoldstein@gmail.com>
2022-12-11 22:54:55 -0700

    Moving responsive logic to shared styles

--- src/App.svelte
+++ src/App.svelte
@@ -29,47 +29,4 @@
 :global(body) {
   margin: 0;
 }
-
-/*** media queries ***/
-
-/* by height */
-@media (max-height: 400px) {
-  :global(#header) {
-    height: 20vh;
-    padding: 5vh;
-  }
-}
-
-@media (min-height: 400px) and (max-height: 600px) {
-  :global(#header) {
-    height: 10vh;
-    padding: 2vh;
-  }
-}
-
-@media (min-height:600px) {
-  :global(#header) {
-    height: 5vh;
-    padding: 2vh;
-  }   
-}
-
-/* by width */
-@media (max-width: 600px) {
-  :global(#header-nav-container) {
-    display: block;
-  }
-}
-
-@media (min-width: 600px) and (max-height: 1000px) {
-  :global(#header-nav-container) {
-    display: flex;
-  }
-}
-
-@media (min-width: 1000px) {
-  :global(#header-nav-container) {
-    display: flex;
-  }   
-}
 </style>
--- src/styles.js
+++ src/styles.js
@@ -1,7 +1,23 @@
 import { css } from '@emotion/css';
 
 /*** breakpoints and responsiveness ***/
+export const breakpoints = {
+	height : {
+		breakpoint0 : '400px',
+		breakpoint1 : '800px',
+	},
+	width : {
+		breakpoint0 : '800px',
+		breakpoint1 : '1200px'
+	}
+}
 
+/*** general sizes ***/
+export const sizes = {
+	headerMaxHeight0 : '20vh',
+	headerMaxHeight1 : '10vh',
+	headerMaxHeight2 : '5vh'
+}
 
 /*** colors ***/
 export const colors = {
@@ -10,23 +26,53 @@ export const colors = {
 	headerNavItemBackgroundColorHover : 'red'
 };
 
+/*** UI elements ***/
 export const headerClasses = {
 	header : css`
+	@media (max-height: ${breakpoints.height.breakpoint0}) {
+		height: ${sizes.headerMaxHeight0};
+		padding: 5vh;
+		}
+	@media (min-height: ${breakpoints.height.breakpoint0}) and (max-height: ${breakpoints.height.breakpoint1}) {
+		height: ${sizes.headerMaxHeight1};
+		padding: 2vh;
+		}
+	@media (min-height: ${breakpoints.height.breakpoint1}) {
+		height: ${sizes.headerMaxHeight2};
+		padding: 2vh;
+		}
+
 	background-color: ${colors.headerBackgroundColor};
 	`,
 	headerLogo : css`
 	height: 100%; /* fill the header with the logo top-to-bottom */
 	`,
 	headerNavContainer : css`
+	/* responsive behavior - by width */
+	@media (max-width: ${breakpoints.width.breakpoint0}) {
+		display: block;
+	  }
+	  @media (min-width: ${breakpoints.width.breakpoint0}) and (max-height: ${breakpoints.width.breakpoint1}) {
+		display: flex;
+	  }
+	  
+	  @media (min-width: ${breakpoints.width.breakpoint1}) {
+		  display: flex;
+	  }
+
 	height: 100%;
 	display: flex;
 	float: right;
 	align-items: center;
 `,
 	headerNavItem : css`
+	display: flex;
+	align-items: center;
 	margin-left: 1rem;
 	margin-right: 1rem;
-	padding: 0.75rem;
+	padding-left: 1rem;
+	padding-right: 1rem;
+	height: 50%;
 	color: ${colors.headerBackgroundColor};
 	background-color: ${colors.headerNavItemBackgroundColor};
 	:hover {
